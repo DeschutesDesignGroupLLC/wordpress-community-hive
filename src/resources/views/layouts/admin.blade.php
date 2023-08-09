68ada76d8b40f04750afc1c4758baefb682235bb
Jon Erickson <jon@deschutesdesigngroup.com>
2023-08-08 22:23:45 -0600

    Add package script

--- src/resources/views/layouts/admin.blade.php
+++ src/resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
     <h1 class='text-xl font-bold pb-4'>{{ $title ?? 'Settings' }}</h1>
     <div class='bg-white p-4 ring-1 ring-black ring-opacity-10 shadow-md rounded-md text-slate-600'>
         @yield('content')
-        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
+        <hr class="h-px my-4 bg-gray-200 border-0">
         <div class='flex justify-center items-center space-x-4'>
             <a href='https://www.communityhive.com/privacy' target='_blank'>Community Hive Privacy Policy</a>
             <a href='https://www.communityhive.com/' target='_blank'>CommunityHive.com</a>
