a29a2c08f815caa75988374d421ce20053fe48ef
Jon Erickson <jon@deschutesdesigngroup.com>
2023-09-01 04:59:35 -0700

    Remove Symfony from namespacing and add an exclusion filter for symfony's error handler view files (#4)

--- scoper.inc.php
+++ scoper.inc.php
@@ -14,8 +14,9 @@ return [
         Finder::create()
             ->files()
             ->ignoreVCS(true)
-            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
+	        ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock|Dockerfile|docker-compose.yml/')
             ->exclude([
+				'bin',
                 'doc',
                 'test',
                 'test_old',
@@ -26,25 +27,27 @@ return [
             ->in('vendor'),
         Finder::create()->append([
             'communityhive.php',
-            'composer.json'
-        ])
+            'composer.json',
+        ]),
     ],
     'exclude-files' => [
         ...array_values(array_map(
-	        static fn (SplFileInfo $fileInfo) => $fileInfo->getPathName(),
-	        iterator_to_array(
-		        Finder::create()
-		              ->files()
-		              ->in('vendor/illuminate/console/resources/views/components')
-	        ),
-        ))
+            static fn (SplFileInfo $fileInfo) => $fileInfo->getPathName(),
+            iterator_to_array(
+                Finder::create()
+                    ->files()
+                    ->in('vendor/illuminate/console/resources/views/components')
+                    ->in('vendor/symfony/error-handler/Resources/views')
+            ),
+        )),
     ],
     'patchers' => [],
     'exclude-namespaces' => [
         'CommunityHive\App',
         'CommunityHive\Database',
         'CommunityHive\Tests',
-        'Illuminate'
+        'Illuminate',
+        'Symfony',
     ],
     'exclude-classes' => ['WP', 'WP_Theme', 'WP_CLI', 'WP_User'],
     'exclude-functions' => ['get_theme_file_path', 'apply_filters'],
