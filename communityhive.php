953416b9752a633ceda4b18eaa55fffb257237a4
Jon Erickson <jon@deschutesdesigngroup.com>
2023-10-20 13:13:08 -0700

    Update to 1.0.3

--- communityhive.php
+++ communityhive.php
@@ -4,7 +4,7 @@
  * Author: Community Hive
  * Author URI: https://www.communityhive.com
  * Description: Community Hive allows your members to follow their favorite communities to receive updates.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Requires PHP: 8.1
  */
 
@@ -31,7 +31,7 @@ if (file_exists(__DIR__ . '/src/.env')) {
 Config::define('ACORN_BASEPATH', rtrim(plugin_dir_path(__FILE__).'src', '/'));
 Config::define('WP_ENV', env('APP_ENV', 'production'));
 Config::define('COMMUNITY_HIVE_BASE_URL', env('COMMUNITY_HIVE_BASE_URL', 'https://www.communityhive.com/api/v1/'));
-Config::define('COMMUNITY_HIVE_PLUGIN_VERSION', '1.0.2');
+Config::define('COMMUNITY_HIVE_PLUGIN_VERSION', '1.0.3');
 Config::define('COMMUNITY_HIVE_PLUGIN_FILE', __FILE__);
 Config::define('COMMUNITY_HIVE_PLUGIN_URL', plugin_dir_url(__FILE__));
 Config::apply();
